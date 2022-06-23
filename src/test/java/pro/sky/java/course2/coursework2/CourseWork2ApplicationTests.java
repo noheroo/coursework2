a73bca2499422c71645316618e6f9d675843836f
nohero_ <serega1552@yandex.ru>
2022-06-23 08:58:36 +0400

    Initial commit

---
+++ src/test/java/pro/sky/java/course2/coursework2/CourseWork2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.coursework2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
